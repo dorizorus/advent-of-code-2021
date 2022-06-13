4d84f9b747870dc79c5365363450c319e792fdd4
dri <ab.blackbird@gmail.com>
2022-06-14 00:08:50 +0200

    Day3.1 working, prep for Day3.2

--- src/main/kotlin/day3/Day3.kt
+++ src/main/kotlin/day3/Day3.kt
@@ -3,50 +3,94 @@ package day3
 import java.io.File
 import kotlin.math.pow
 
-// really messy stuff. I had an idea, it was awful in the end.
-fun Map<Int,Int?>.getGammaTimesEpsilonRayResult(inputSize : Int) : Double {
-    var gamma = 0.0
-    var epsilon = 0.0
-    println(inputSize)
-
-    if (inputSize % 2 == 0)
-        for (key in this.keys) {
-            println("key  $key || value ${this[key]}")
-                if (this[key]!! >= (inputSize / 2))
-                    gamma += 2.0.pow(key.toDouble())
+fun Map<Int,Int?>.getStringOfMostPopularBit(inputSize : Int) : String {
+    var string = ""
+    when (inputSize.hasEvenEntries()) {
+        true -> {
+            for (key in this.keys)
+                string += if (is1MostPopularBit_EvenSize(this[key], inputSize))
+                    "1"
                 else
-                    epsilon += 2.0.pow(key.toDouble())
+                    "0"
         }
-    else
-        for (key in this.keys) {
-            println("key  $key || value ${this[key]}")
-            if (this[key]!! >= (inputSize + 1 / 2))
-                gamma += 2.0.pow(key.toDouble())
-            else
-                epsilon += 2.0.pow(key.toDouble())
-        }
+            false -> {
+                for (key in this.keys)
+                    string += if (is1MostPopularBit_OddSize(this[key], inputSize))
+                        "1"
+                    else
+                        "0"
+            }
+    }
 
-        println("gamma : $gamma || epsilon : $epsilon")
-
-
-    return gamma * epsilon
+    return string
 }
+
+fun String.invertBits() : String {
+    var string = ""
+    for (char in this)
+        string += if (char == '0')
+            "1"
+        else
+            "0"
+    return string
+}
+
+fun List<String>.`find CO2 scrubber rating`(gammaString : String) : String {
+    return "0"
+}
+
+fun List<String>.`find oxygen generator rating`(epsilonString : String) : String {
+    return "0"
+}
+
+fun String.convertBinaryToDecimal() : Int {
+    var decimal = 0.0
+    for (i in this.indices)
+        decimal += this[i].toString().toDouble() * 2.0.pow(this.length - 1 - i)
+    return decimal.toInt()
+}
+
+
+fun Int.hasOddEntreies() : Boolean = this % 2 != 0
+fun Int.hasEvenEntries() : Boolean = this % 2 == 0
+fun is1MostPopularBit_EvenSize(input : Int?, size : Int) : Boolean = input!! >= size / 2
+fun is1MostPopularBit_OddSize(input : Int?, size : Int) : Boolean = input!! >= (size+1) / 2
 
 fun main(args: Array<String>) {
     val input = File("""src\main\kotlin\day3\input.txt""").readLines().map {it}
-    val mapOfBits = mutableMapOf<Int, Int?>()
+
+    /**
+     * Goal is :
+     * 1. get a map of <Int,Int>
+     *     The first Int is the position of the bit
+     *     The second Int is a counter of how many "1"s there are in at that pos in the list of inputs
+     * 2. if the counter of "1"s is >= input.size / 2 or is >= (input.size+1) / 2 (depending on even or odd number of entries)
+     * this means 1 is the most popular else it's 0
+     * 3. construct a string of most popular bits (gammaString) and its opposite (epsilonString)
+     * 4. work from there to get the answer of part1 (which is just converting them to decimal and multiplying them)
+     *      and of part2 (go through the list.. again ! to sort these out)
+     */
+    val mapOfBits = mutableMapOf<Int, Int>()
+
     for (sequence in input) {
         for (i in sequence.indices) {
             val bitCounter = when (mapOfBits[i]) {
                 null -> sequence[i].toString().toInt()
-                else -> mapOfBits[i]?.plus(sequence[i].toString().toInt())
+                else -> mapOfBits[i]!!.plus(sequence[i].toString().toInt())
             }
             mapOfBits[i] = bitCounter
         }
     }
 
-    val answer1 = mapOfBits.getGammaTimesEpsilonRayResult(input.size)
-    println(answer1) //1471406
+    val gammaString = mapOfBits.getStringOfMostPopularBit(input.size)
+    val epsilonString = gammaString.invertBits()
+
+    val answer1 = gammaString.convertBinaryToDecimal() * epsilonString.convertBinaryToDecimal()
+    println(answer1) //4138664
+
+
+    val answer2 = 0
+    println(answer2) // XXX
 
 
 
